b3974ee8498647dba54d9e9b7e190f16b75bdc9d
laykith <laykith@gmail.com>
2018-11-20 11:43:34 +0100

    Added function to check game state

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function createCard(value, img){
-    return `<div class="card flipped" data-value="${value}">${img}</div>`
+    return `<div class="card" data-value="${value}">${img}</div>`
 }
 
 function startGame(gameboard, pairCount){
@@ -15,4 +15,29 @@ function startGame(gameboard, pairCount){
 function resetGame(gameboard, pairCount){
     gameboard.innerHTML = ''
     startGame(gameboard, pairCount)
+}
+
+//Check the state of the game
+function checkState(){
+    const cards = document.querySelectorAll('.card')
+    const activeCards = document.querySelectorAll('.active')
+    
+    //Compare selected card values and complete them on match
+    if(activeCards.length === 2){
+        if(activeCards[0].dataset.value === activeCards[1].dataset.value){
+            activeCards[0].classList.add('completed')
+            activeCards[1].classList.add('completed')
+        }
+
+        //Unselect selected cards
+        activeCards.forEach(function(card){
+            card.classList.remove('active')
+        })
+    }
+
+    //Check if game is completed
+    const completedCards = document.querySelectorAll('.completed')
+    if(completedCards.length === cards.length){
+        console.log('Game won!')
+    }
 }
