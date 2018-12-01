1b81110b0460f42a15975a88c3eec2daa8bcd8b2
laykith <laykith@gmail.com>
2018-12-01 16:40:24 +0100

    Added difficulty selectors

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 const gameboard = document.querySelector('#gameboard')
-let pairCount = 6
 
-startGame(gameboard, pairCount)
+const difficulty = document.querySelectorAll('.difficulty-selector')
+
+const pairCount = {
+    easy: 6,
+    medium: 7,
+    hard: 8
+}
+
+let setDifficulty = "easy"
+
+difficulty.forEach(function(item){
+    item.addEventListener('click', function(){
+        setDifficulty = this.id;
+        resetGame(gameboard, pairCount[setDifficulty])
+    })
+})
+
+startGame(gameboard, pairCount[setDifficulty])
 
 
 //Register Reset button
 const reset = document.querySelector('#reset')
 
-reset.addEventListener('click', function(){resetGame(gameboard, pairCount)})
+reset.addEventListener('click', function(){resetGame(gameboard, pairCount[setDifficulty])})
